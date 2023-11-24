c5bef4a66b6fa611871fb8e14450198f175caad6
playerking3 <murilomarquesdecastro@gmail.com>
2023-11-24 16:20:37 -0300

    logica feita

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+function avalia(elemento){
+    let pai = elemento.parentElement;
+    let qty = pai.childElementCount;
+    let chave = false
+    for (let i = 0; i < qty; i++){
+        if (pai.children[i] != elemento && chave == false){
+            pai.children[i].className = 'fa-solid fa-star fa-lg'
+        }else if (pai.children[i] == elemento){
+            pai.children[i].className = 'fa-solid fa-star fa-lg'
+            chave = true
+        }else if (pai.children != elemento && chave == true){
+            pai.children[i].className = 'fa-regular fa-star fa-lg'
+        }
+    }
+}
